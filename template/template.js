d481fa209dc2815a6fecbf49c708555086c081e1
Dafa Bintang Athaillah <Dafabintang76@gmail.com>
2022-02-14 13:25:45 +0800

    Jobsheet 1 Pengenalan JavaSCript

---
+++ template/template.js
@@ -0,0 +1,7 @@
+let fname = 'Cristiano';
+let lname = 'Ronaldo';
+let age = prompt("Masukkan Umur Cristiano Ronaldo!");
+
+//memakai template string
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
